f5c20405ac78afe26152404ca5e7a813cca0f21b
DevWonny <cjfdnjs1994@naver.com>
2022-07-10 12:02:49 +0900

    docs : 주석 추가

--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -32,7 +32,9 @@ const Main = () => {
 
   // intersection
   const onIntersect = async ([entry], observer) => {
+    console.log('test');
     if (entry.isIntersecting && !isLoading) {
+      // IntersectionObserver.unboserver -> 타겟 엘리먼트에 대한 관찰을 멈추고 싶을 때 사용.
       observer.unobserve(entry.target);
       await infiniteFetchPost();
       observer.observe(entry.target);
@@ -40,12 +42,19 @@ const Main = () => {
   };
 
   useEffect(() => {
+    // 처음 target이 변경 되기때문에 초기1회 실행이 됨.
+    // 그래서 따로 초기 실행 함수가 필요 없음
+
     let observer;
 
     if (!!target) {
+      // IntersectionObserver(callback, options)
+      // callback -> target Element가 교차되었을 때 실행할 함수
       observer = new IntersectionObserver(onIntersect, {
         threshold: 0.4,
       });
+
+      // IntersectionObserver.observe(targetElement) -> target Element에 대한 IntersectionObserver를 관찰을 시작할때 사용.
 
       observer.observe(target);
     }
